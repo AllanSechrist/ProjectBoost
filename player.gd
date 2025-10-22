87e1ccb9ff8b2bc782cf91f84ce9d5ccb62d7ea2
Allan Sechrist <sechristallan@gmail.com>
2025-10-22 22:39:07 +0900

    add level 2 and level 3

--- player.gd
+++ player.gd
@@ -1,19 +1,31 @@
 extends RigidBody3D
 
+## How much vertical force to apply while moving
+@export_range(750.0, 3000.0) var thrust: float = 1000.0
+@export var torque_thrust: float = 100.0
+
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
-		apply_central_force(basis.y * delta * 1000.0)
+		apply_central_force(basis.y * delta * thrust)
 	
 	if Input.is_action_pressed("rotate_left"):
-		apply_torque(Vector3(0.0, 0.0, 100.0 * delta))
+		apply_torque(Vector3(0.0, 0.0, torque_thrust * delta))
 		
 	if Input.is_action_pressed("rotate_right"):
-		apply_torque(Vector3(0.0, 0.0, -100.0 * delta))
+		apply_torque(Vector3(0.0, 0.0, -torque_thrust * delta))
 
 
 func _on_body_entered(body: Node) -> void:
 	if "Goal" in body.get_groups():
-		print("You win!")
+		complete_level(body.file_path)
 	elif "Fail" in body.get_groups():
-		print("You lose!")
+		crash_sequence()
+		
+func complete_level(next_level_file) -> void:
+	print("You win!")
+	get_tree().change_scene_to_file(next_level_file)
+
+func crash_sequence() -> void:
+	print("KABOOM!")
+	get_tree().reload_current_scene()
